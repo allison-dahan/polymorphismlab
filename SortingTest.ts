c1474b04067f8085d4ec2856832bf70498243583
Allison Dahan <allison.dahan@yahoo.ca>
2024-01-30 12:05:27 -0800

    second test

--- SortingTest.ts
+++ SortingTest.ts
@@ -4,7 +4,7 @@ import { NumberGroup } from "./NumberGroup";
 import { SortUtil } from "./SortUtil";
 
 
-const numberGroup = new NumberGroup([30, 3, -15, 0as]);
+const numberGroup = new NumberGroup([30, 3, -15, 0]);
 const sorter1 = new SortUtil(numberGroup);
 sorter1.sort();
 console.log(numberGroup.data);
